f5cab5639e6db7cfa2dfa8a78f69d62143b768fc
Steven Rodriguez <71090662+estiven94ops@users.noreply.github.com>
2022-01-25 22:32:53 -0500

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -1,25 +1,37 @@
-import java.io.InputStream;   import java.net.ServerSocket;   import java.net.Socket;
+import java.io.InputStream; import java.net.ServerSocket; import java.net.Socket; import java.io.DataInputStream;
 
 public class TCPServer {
-  public static final void main(final String[] args) {
-    ServerSocket server;           InputStream  is;
-    Socket       client;          
+    public static final void main(final String[] args) {
+    ServerSocket server;
+    Socket client;
+    //InputStream is;
+    InputStream is;
+    int n;
 
     try {
-      server = new ServerSocket(9999);//(*@\serverBox{1 + 2)}@*)
-      
-      for (int j = 5; (--j) >= 0;) {  //process only 5 clients, so I can show (*@\serverBox{5)}@*) below
-        client = server.accept(); //wait for incoming connection (*@\serverBox{3)}@*)
-        System.out.println("New connection from " + client.getRemoteSocketAddress());
-        
-        is     = client.getInputStream(); //get stream to read from
-        System.out.println(is.read());//(*@\serverBox{4} + \clientBox{3})@*)
-             
-        client.close(); //close connection to client
-      }
-      server.close();   //(*@\serverBox{5)}@*)
-    } catch (Throwable t) {
-      t.printStackTrace();
+        server = new ServerSocket(5000);
+        System.out.println("servidor iniciado");
+
+        for (int j = 5; (--j) >= 0;){
+            client = server.accept();
+
+            is = client.getInputStream();
+            n = is.read();
+
+            for(int i = 0; i < n; i++){
+                System.out.print("X");
+            }
+            System.out.print('\n');
+
+            client.close();
+            }
+        server.close();
+
+        } catch (Throwable t){
+            t.printStackTrace();
+        }
+
     }
-  }
+
+
 }
